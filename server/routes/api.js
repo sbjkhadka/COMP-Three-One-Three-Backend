32fb67d46e1ad76f54c363ebaa16141dde1929d7
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-10-12 06:41:37 -0400

    Created route for login

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,9 +6,9 @@ router.get('/', (req, res) => {
     res.send('Testing... testing');
 });
 
+// Route for registering a user
 router.post('/', async(req, res)=> {
     const{first_name, last_name, email, role, password} = req.body;
-    console.log('request12345', req.body);
     let user = {};
     user.first_name = first_name;
     user.last_name = last_name;
@@ -17,8 +17,42 @@ router.post('/', async(req, res)=> {
     user.password = password;
 
     let userModel = new User(user);
-    await userModel.save();
-    res.json(userModel);
+    await userModel.save()
+    .then((user) => {
+        res.json({status: "OK", user: user});
+    })
+    .catch(error => {
+      const tempObj = { ...error };
+      delete tempObj.keyValue;
+      res.json({ status: "FAIL", details: tempObj }); // handle this from the backend
+    });
+});
+
+// Route for login
+router.post('/login', async(req, res) => {
+    const email = req.body.email;
+    await User.findOne({ email: email })
+      .then((user) => {
+          if(user) {
+               res.json({
+                 status: "OK",
+                 user: {
+                   email: user.email,
+                   firstName: user.first_name,
+                   last_name: user.last_name,
+                   role: user.role,
+                 },
+               });
+          } else {
+              res.json({status: "FAIL", details: "User not found"});
+          }
+      })
+      .catch((error) => {
+        const tempObj = { ...error };
+        delete tempObj.keyValue;
+        console.log('err', error.message)
+        res.json({ status: "FAIL", details: tempObj }); // handle this from the backend
+      });
 });
 
 module.exports = router;
