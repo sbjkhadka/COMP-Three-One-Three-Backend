4f79ce0b3903e09dcff77cb8549e61555d41308d
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-10-12 04:03:05 -0400

    Created DB connection with mongoDB Atlas

---
+++ server/DB/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const URI = process.env.MONGODB_URI;
+
+// Connect to the mongoDB 
+const connectDB = async() => {
+    await mongoose.connect(URI);
+    console.log('Database connected')
+};
+
+module.exports = connectDB;
