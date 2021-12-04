56205ab00f3cb6a17ab40a687de96f2c6559b5de
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-12-04 02:25:44 -0500

    bugfix feedback schema

--- server/DB/schema/feedback.js
+++ server/DB/schema/feedback.js
@@ -2,20 +2,20 @@
 const mongoose = require('mongoose');
 
 const feedback = new mongoose.Schema({
-    type: {
-        type: String,
-    },
-    message: {
-        type: String,
-    },
-    userEmail: {
-        type: String,
-        enum: ['Support', 'Feedback']
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-    }
+  type: {
+    type: String,
+    enum: ["Support", "Feedback"],
+  },
+  message: {
+    type: String,
+  },
+  userEmail: {
+    type: String,
+  },
+  user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+  }
 });
 
 module.exports = User = mongoose.model('feedback', feedback);
