b9cdf50c50813c3ef85e71259b8166b326cfec27
Lushi <vankovc@gmail.com>
2021-12-12 11:18:30 -0500

    Added comments to routes and deleted commented out code

--- server/controllers/recipe.controler.js
+++ server/controllers/recipe.controler.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const Recipe = require("mongoose").model("RecipeModel");
+const Recipe = require('../DB/schema/recipeModel')
 
 const router = express.Router();
 
-export const getGlobalRecipes = async (req, res) => {
-    const filters = req.query;
-    const recipes = await Recipe.find();
-    const filterRecipe = recipes.filter((recipe) => recipe.isGlobal === true)
-
-    if (filterRecipe) {
-        res.json({ status: 200, filterRecipe: filterRecipe });
-    } else {
-        res.json({ status: 404, details: 'recipes not found' })
-    }
-};
+module.exports.deleteRecipe = async (recipeId) => {
+    Recipe.deleteOne({ _id: recipeId }).then((response) => {
+        return {
+            status: 200,
+            userId: response._id
+        }
+    })
+        .catch(error => {
+            throw error;
+        });
+}
